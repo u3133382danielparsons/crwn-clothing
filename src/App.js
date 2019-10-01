08d3ac53581ebe5e97472b2af10092cb12cd6265
Daniel Parsons <danielparsons3133382@gmail.com>
2019-10-01 16:04:54 +1000

    Added a redirect to homepage on sign in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
@@ -45,8 +45,11 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route   path='/shop' component={ShopPage} />
-          <Route   path='/signin' component={SignInAndSignUpPage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route 
+            exact path='/signin' 
+            render={() => this.props.currentUser ? (<Redirect to='/' />) : <SignInAndSignUpPage />} 
+          />
   
         </Switch>
       </div>
@@ -54,8 +57,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
